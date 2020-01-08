134dc16b0d7c86d51f736948d0e90b8f076bb92b
Alex Lementuev <a.lementuev@gmail.com>
2020-01-08 14:06:35 -0800

    Solved "multiply"

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -2,8 +2,34 @@ from test_framework import generic_test
 
 
 def multiply(num1, num2):
-    # TODO - you fill in here.
-    return []
+    def sign(val):
+        return 1 if val >= 0 else -1
+
+    s = sign(num1[0]) * sign(num2[0])
+    i = len(num2) - 1
+    k = 0
+    res = [0] * (len(num1) + len(num2))
+    carry = 0
+    while i >= 0:
+        d1 = abs(num2[i])
+        j = len(num1) - 1
+        t = k
+        while j >= 0 or carry > 0:
+            prod = (d1 * abs(num1[j])) if j >= 0 else 0
+            temp = res[t] + prod + carry
+            res[t] = temp % 10
+            carry = temp // 10
+            j -= 1
+            t += 1
+        k += 1
+        i -= 1
+
+    while len(res) > 1 and res[-1] == 0:
+        res.pop()
+
+    res.reverse()
+    res[0] *= s
+    return res
 
 
 if __name__ == '__main__':
